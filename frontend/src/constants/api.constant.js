85fa7755d3a129bbba45a6ee45878812040cf6a8
Samsul Subakti <samsulsubakti@62teknologi.com>
2024-11-25 22:39:57 +0700

    update 2 files and delete 424 files

--- frontend/src/constants/api.constant.js
+++
@@ -1,3 +0,0 @@
-export const TOKEN_TYPE = "Bearer ";
-export const REQUEST_HEADER_AUTH_KEY = "Authorization";
-export const API_URL = process.env.REACT_APP_API_HOST;
--- frontend/src/constants/app.constant.js
+++
@@ -1,4 +0,0 @@
-export const APP_NAME = "62Teknologi";
-export const PERSIST_STORE_NAME = "admin";
-export const REDIRECT_URL_KEY = "redirectUrl";
-export const HOST_URL = process.env.REACT_APP_BASE_HOST;
